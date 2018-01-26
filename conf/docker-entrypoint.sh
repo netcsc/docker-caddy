72fdde56e9950839ee31085be4810a9ff2b2ee33
Shi Chen <shichen.sean@gmail.com>
2018-01-26 15:01:02 -0500

    update caddyfile

--- conf/docker-entrypoint.sh
+++ conf/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 
 cat /etc/caddy/Caddyfile |\
     sed "s@#CADDY_ADMIN_USER#@$CADDY_ADMIN_USER@g" |\
-    sed "s@#CADDY_ADMIN_PASSWORD#@$CADDY_ADMIN_PASSWORD@g" > /tmp/Caddyfile
+    sed "s@#CADDY_ADMIN_PASSWORD#@$CADDY_ADMIN_PASSWORD@g" |\
+    sed "s@#PROMETHEUS#@$PROMETHEUS@g" > /tmp/Caddyfile
 
 mv /tmp/Caddyfile /etc/caddy/Caddyfile
 
